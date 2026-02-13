2256cd55b76ffe7496e7dee4d97dc69937633d8b
sandeshsolanki <sandeshsolanki4@gmail.com>
2026-02-13 18:35:57 +0530

    add one more program

---
+++ src/arrays/findMajorityElement.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class findMajorityElement {
+    public static int majorityElement(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+
+            if (count > n / 2) {
+                return nums[i];
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,1,1,1,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3};
+
+        System.out.println(majorityElement(arr));
+    }
+}
