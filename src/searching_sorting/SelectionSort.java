c22ae468b27a33085676399d15dc692f8e3184a6
sandeshsolanki <sandeshsolanki4@gmail.com>
2026-02-17 12:19:38 +0530

    add selection sort program

---
+++ src/searching_sorting/SelectionSort.java
@@ -0,0 +1,41 @@
+package searching_sorting;
+
+import javax.management.remote.rmi.RMIJRMPServerImpl;
+
+public class SelectionSort {
+    public static void selection(int[] arr)
+    {
+        for (int i=0; i<arr.length-1; i++)
+        {
+            int minPos = i;
+            for (int j=i+1; j<arr.length; j++)
+            {
+                if (arr[minPos] > arr[j])
+                {
+                    minPos = j;
+
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArr(int[] arr)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {7,3,4,1,0,12,65,11,23};
+        selection(arr);
+        printArr(arr);
+    }
+
+}
