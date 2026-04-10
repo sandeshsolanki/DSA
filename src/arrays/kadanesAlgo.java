2a24fa8f9e7d2d8391649c173b994df0413ba53a
sandeshsolanki <sandeshsolanki4@gmail.com>
2026-04-10 10:11:35 +0530

    add new program

--- src/arrays/kadanesAlgo.java
+++ src/arrays/kadanesAlgo.java
@@ -14,7 +14,7 @@ public class kadanesAlgo {
     }
 
     public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr = {-2, -1, -3, -4, -1, -2, -1, -5, -4};
         System.out.println( maxSubArray(arr));
     }
 }
